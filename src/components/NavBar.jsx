e7570be1f678b0765b3b929eb864d1414b6531c0
jerra <9416924+jerra@users.noreply.github.com>
2026-09-04 08:48:15 +0200

    Add theme functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,44 @@ import { useState } from 'react';
 
 export default function NavBar() {
   const [light, setLight] = useState(true);
-  const body = document.body;
+  const theme = light === true ? 'light' : 'dark';
 
   const handleThemeSwitch = () => {
-    const nextTheme = setLight(light === true ? 'dark' : 'light');
-    body.classList.replace()
+    setLight(!light);
+    document.body.className = light === true ? 'dark' : 'light';
   } 
 
 
-  return(
-    <nav className="navbar">
-      NAVBAR
-    </nav>
+return (
+    <div className="navbar">
+      <a className="logo">
+        SuperM
+      </a>
+      <nav className="nav-wrapper">
+        <button onClick={handleThemeSwitch} className="theme-switcher">
+          <img
+            src={`https://res.cloudinary.com/dbfn5lnvx/image/upload/v1732813039/react-tutorial/superm-v2/${theme}.svg`}
+            width="24"
+            height="24"
+            alt={`${theme} theme`}
+          />
+        </button>
+        <ul className="nav">
+          <li className="nav-item">
+            <a>Home</a>
+          </li>
+          <li className="nav-item">
+            <a>Login</a>
+          </li>
+          <li className="nav-item">
+            <a>Products</a>
+          </li>
+        </ul>
+        <a className="btn btn-nav">
+          Cart (0)
+        </a>
+      </nav>
+    </div>
   );
 
 }
